17d570340f7d93231354656ee82384dac53f32c4
Srinjoy <srinjoypramanik2004@gmail.com>
2025-04-16 23:00:05 +0530

    removed few unnecessary files

--- retell api/createllm.py
+++
@@ -1,7 +0,0 @@
-from retell import Retell
-
-client = Retell(
-    api_key="key_367e624c5c5527a8ea2c8b193a6d",
-)
-llm_response = client.llm.create()
-print(llm_response.llm_id)
--- retell api/voiceid.py
+++
@@ -1,9 +0,0 @@
-from retell import Retell
-
-client = Retell(
-    api_key="key_367e624c5c5527a8ea2c8b193a6d",
-)
-voice_response = client.voice.retrieve(
-    "11labs-Adrian",
-)
-print(voice_response.provider)
